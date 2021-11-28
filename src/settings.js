414cae8d9db97291bb76fccb1561f507a460df30
osmo19 <93938600+osmo19@users.noreply.github.com>
2021-11-28 10:38:53 +0700

    Add files via upload

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",Game:"12a95",main:"7d035"}};
